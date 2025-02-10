9429e8be8cd821a2480b52f8a0b31fd1e6c9d070
rsvofc <161730170+rsvofc@users.noreply.github.com>
2025-02-10 17:27:03 +0700

    Update install.sh

--- v1/install.sh
+++ v1/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 apt --fix-missing update && apt update && apt upgrade -y
-wget -q -O install https://raw.githubusercontent.com/rsvofc/new/refs/heads/main/v1/install.sh
+wget -q -O install https://raw.githubusercontent.com/rsvofc/new/refs/heads/main/v1/rsv.sh
 chmod +x install
 ./install
